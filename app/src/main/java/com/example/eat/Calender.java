813e2ad1225f9f0d9a567fb42288303b54adc729
parkhyunjun99 <wprzl479@naver.com>
2023-06-04 00:10:11 +0900

    데베 연동 됨 / 체크리스트 형식으로 됨 / 캘린더 연동 됨 / 캘린더 날짜 바뀔 때 마다 체크박스 초기화

--- app/src/main/java/com/example/eat/Calender.java
+++ app/src/main/java/com/example/eat/Calender.java
@@ -6,18 +6,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.CalendarView;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.myapplication.R;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Calender extends AppCompatActivity {
 
     CalendarView cal;
-    TextView tv_text, textView2;
+    TextView tv_text;
+    private LinearLayout layout;
+
+    // 체크박스들을 저장할 리스트를 선언
+    private List<CheckBox> checkBoxes = new ArrayList<>();
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -25,28 +34,46 @@ public class Calender extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calender);
 
+        layout = findViewById(R.id.calenderLayout);
+
+        // SharedPreferences에서 CheckBox 텍스트 불러오기
+        SharedPreferences checkBoxPrefs = getSharedPreferences("CheckBoxes", Context.MODE_PRIVATE);
+        Map<String, ?> allEntries = checkBoxPrefs.getAll();
+
+        // 모든 약의 이름에 대해 체크박스를 생성합니다.
+        for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+            String itemName = entry.getValue().toString();
+
+            // 동적으로 체크리스트 생성
+            CheckBox checkBox = new CheckBox(this);
+            checkBox.setText(itemName);
+            checkBox.setTextColor(Color.BLACK);
+            checkBox.setTextSize(18);
+
+            // LinearLayout에 체크리스트 추가
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            layout.addView(checkBox, layoutParams);
+
+            // 생성한 체크박스를 리스트에 추가
+            checkBoxes.add(checkBox);
+        }
+
         cal = findViewById(R.id.cal);
         tv_text = findViewById(R.id.tv_text);
-        textView2 = findViewById(R.id.textView2);
 
         cal.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int day) {
-                tv_text.setText( year + "년" + (month + 1) + "월" + day + "일");
+                tv_text.setText(year + "년" + (month + 1) + "월" + day + "일");
+
+                // 날짜가 변경될 때마다 체크박스를 모두 해제
+                for (CheckBox checkBox : checkBoxes) {
+                    checkBox.setChecked(false);
+                }
             }
         });
-
-        // SharedPreferences에서 체크박스 텍스트 가져오기
-        SharedPreferences checkBoxPrefs = getSharedPreferences("CheckBoxes", Context.MODE_PRIVATE);
-        Map<String, ?> allEntries = checkBoxPrefs.getAll();
-
-        StringBuilder checkBoxTexts = new StringBuilder();
-        for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
-            checkBoxTexts.append(entry.getValue()).append("\n");
-        }
-
-        // 체크박스 텍스트를 TextView에 설정
-        textView2.setText(checkBoxTexts.toString());
     }
 }
